c4bfcdd8b7e958f2ae20bec6af4adafb382c07f9
Oyetunji <oyetunjilukman@gmail.com>
2024-08-08 05:48:49 +0100

    setup pipeline

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-CHECK=$(ps aux | grep myWebApp | grep -v grep)
-if [ ""="$CHECK"]; 
-    then
-        echo "myWebApp is not running"
-    else
-        systemctl stop myWebApp
-fi
+# CHECK=$(ps aux | grep myWebApp | grep -v grep)
+# if [ ""="$CHECK"]; 
+#     then
+#         echo "myWebApp is not running"
+#     else
+#         systemctl stop myWebApp
+# fi
+
+echo "first deployment"
